e000b67570a35d3e8aba3093a9e699ae238e1c64
yaoxiaoqi <yaoxiaoqi123@gmail.com>
2018-11-23 16:02:55 +0800

    init project

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by 妖小七 on 2018/11/23.
+//  Copyright © 2018 妖小七. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
